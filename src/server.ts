7ca2207a007f0ecb1917d9aecd4f363defc756e7
BrenDev0 <lahey1991@gmail.com>
2025-03-28 12:33:20 -0600

    listings router

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import listingsRouter from './routes/listings';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/listings", listingsRouter);
 
 const server = () => {
     app.listen(3000, () =>{
