8008fe54753b598d89e29723234729bedb5ca764
BrenDev0 <lahey1991@gmail.com>
2025-04-02 09:45:10 -0600

    helmet

--- dist/server.js
+++ dist/server.js
@@ -6,7 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const listings_1 = __importDefault(require("./routes/listings"));
 const events_1 = __importDefault(require("./routes/events"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const app = (0, express_1.default)();
+app.use((0, helmet_1.default)());
+const limiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+});
+app.use(limiter);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/listings", listings_1.default);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import listingsRouter from './routes/listings';
 import eventsRouter from './routes/events';
-import EventsController from './controllers/EventsController';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
+app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
