9b405742e88a6f26268baca64f3708d69689bbaf
Brendan Lahey <lahey1991@gmail.com>
2025-04-19 13:22:45 -0600

    swagger tags

--- dist/src/swagger/endpoints.js
+++ dist/src/swagger/endpoints.js
@@ -13,11 +13,26 @@ const pool = {};
 const eventsController = new EventsController_1.default(new EventsService_1.default(pool));
 const listingsController = new ListingsController_1.default(new ListingsService_1.default(pool));
 //events
-router.get("/events/read", eventsController.readRequest.bind(eventsController));
-router.get("/events/collection/:agent", eventsController.collectionRequest.bind(eventsController));
-router.get("/events/resource/:id", eventsController.readRequest.bind(eventsController));
-router.post("/events/insert", eventsController.insertRequest.bind(eventsController));
-router.delete("/events/delete/:col/:identifier", eventsController.deleteRequest.bind(eventsController));
+// #swagger.tags 
+router.get("/events/read", 
+// #swagger.tags = ['Events']
+eventsController.readRequest.bind(eventsController));
+router.get("/events/collection/:agent", 
+// #swagger.tags = ['Events']
+eventsController.collectionRequest.bind(eventsController));
+router.get("/events/resource/:id", 
+// #swagger.tags = ['Events']
+eventsController.readRequest.bind(eventsController));
+router.post("/events/insert", 
+// #swagger.tags = ['Events']
+eventsController.insertRequest.bind(eventsController));
+router.delete("/events/delete/:col/:identifier", 
+// #swagger.tags = ['Events']
+eventsController.deleteRequest.bind(eventsController));
 //listings
-router.get("/listings/read", listingsController.readRequest.bind(listingsController));
-router.get("/listings/resource/:id", listingsController.rescourceRequest.bind(listingsController));
+router.get("/listings/read", 
+// #swagger.tags = ['Listings']
+listingsController.readRequest.bind(listingsController));
+router.get("/listings/resource/:id", 
+// #swagger.tags = ['Listings']
+listingsController.rescourceRequest.bind(listingsController));
